d5a6d96d6b0f619ff236386116f8eae3af46d758
csttn <csttn.dev@gmail.com>
2021-04-06 18:28:44 -0300

    add estilos globais

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>csttnews</title>
+        <h1>Csttnews</h1>
+      </Head>
+    </>
+  );
+}
